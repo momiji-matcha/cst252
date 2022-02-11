959c65cbec6c364765b970caf54b98704d209b8d
momiji-matcha <maplematchamomiji@gmail.com>
2022-02-11 14:04:23 -0800

    Lab 5 (pre photo)

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+// Authors: Sugi and Jeremy
+// Created:   02/11/2022
+// License: Public Domain
+
+
+ // Define Variables
+ make = "Honda";
+ model = "Accord";
+ color = "Silver";
+ year = 2001;
+
+ // calculate
+ age = 2022-2001;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>")
