60d98214afa590255c919f9441323ecadfbd8420
momiji-matcha <maplematchamomiji@gmail.com>
2022-04-12 23:08:21 -0700

    Lab 18

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -20,9 +20,9 @@ function Vehicle (make, model, year, color, extras) {
 var cars = []
 
 this.info();
-// cars.push (new Vehicle("Honda", "Accord", " 2001", "Silver", "nothing special"));
-// cars.push (new Vehicle("Toyota", "Prius", "2010", "Gold", "a sunroof"));
-// cars.push (new Vehicle("Acura", "TL", "2005", "Grey", "a sunroof and sports trim"));
+cars.push (new Vehicle("Honda", "Accord", " 2001", "Silver", "nothing special"));
+cars.push (new Vehicle("Toyota", "Prius", "2010", "Gold", "a sunroof"));
+cars.push (new Vehicle("Acura", "TL", "2005", "Grey", "a sunroof and sports trim"));
 
 
 for(var i =0; i<cars.length; i++){
---
+++ lab18/js/lab.js
@@ -0,0 +1,6 @@
+/*
+ * Author: Sugi and Jeremy
+ * Created: 15th March
+ * License: Public Domain
+ */
+ 
