4d02f76807f4077545d1939ef9bef0c8e8c9add5
momiji-matcha <maplematchamomiji@gmail.com>
2022-04-19 22:58:04 -0700

    Lab 19 AJAX

---
+++ lab19/js/lab.js
@@ -0,0 +1,31 @@
+/*
+ * Author: Sugi and Jeremy
+ * Created: 15th March
+ * License: Public Domain
+ */
+
+
+//define a URL
+var url="http://numbersapi.com/random/trivia";
+
+//make the button fetch AJAX
+$("#activate").click(getAjax);
+
+//create AJAX function
+function getAjax () {
+  $.ajax({
+    url: url,
+    type: "GET",
+  })
+
+//if the request succeeds
+  .done(function(data) {
+    console.log(data);
+    $("#output").prepend("<p>" + data);
+  })
+
+//if the request fails
+  .fail(function (xhr, status, errorThrown){
+    console.log(errorThrown + " Staus:" + status);
+})
+}
